c2da3ca21e0adcaf79f07f3128b65df0f3b13d34
Devina <devinardya@gmail.com>
2020-02-22 11:43:53 +0100

    Updates

---
+++ precache-manifest.475356f16a580c2a5f93b843440eded6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d1801725a14cafebccfd42e1cd3983",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "34697569df349f07fd8c",
+    "url": "/todoapp/static/css/main.23bac3a3.chunk.css"
+  },
+  {
+    "revision": "92fe9ebfc9078cdb4575",
+    "url": "/todoapp/static/js/2.70a41fb1.chunk.js"
+  },
+  {
+    "revision": "5d4d8317ad6ab7bc1d5953d14cfdb3f7",
+    "url": "/todoapp/static/js/2.70a41fb1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34697569df349f07fd8c",
+    "url": "/todoapp/static/js/main.1b12534a.chunk.js"
+  },
+  {
+    "revision": "63ed8d88c0187d779e28",
+    "url": "/todoapp/static/js/runtime-main.47b19ebb.js"
+  },
+  {
+    "revision": "46e70a0da343fd56eadf69a494d497ee",
+    "url": "/todoapp/static/media/background-online.46e70a0d.png"
+  },
+  {
+    "revision": "fa9bc3961421013c3388362536b378b0",
+    "url": "/todoapp/static/media/background.fa9bc396.png"
+  }
+]);
